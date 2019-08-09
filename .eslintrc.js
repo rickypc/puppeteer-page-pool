3399c91d14b0e8f247254b0efd6d4d43a4c593f1
Richard Huang <rickypc@users.noreply.github.com>
2019-08-09 13:21:37 -0700

    Adds ESLint config file.

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+/*!
+ *  .eslintrc.js - ESLint configuration.
+ *  Copyright (c) 2018 - 2019 Richard Huang <rickypc@users.noreply.github.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+module.exports = {
+  env: {
+    'jest/globals': true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/recommended',
+  ],
+  plugins: [
+    'import',
+    'jest',
+  ],
+  rules: {
+    'import/no-unresolved': [2, { ignore: ['puppeteer'] }],
+    'no-underscore-dangle': [2, { allow: ['__test__'] }],
+    'space-before-function-paren': ['error', 'always'],
+  },
+}
