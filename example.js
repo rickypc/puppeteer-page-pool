3ce48c47e2cbfc5c1d4f95bc0ea9406c666957e6
Richard Huang <rickypc@users.noreply.github.com>
2019-08-09 22:41:28 -0700

    Adjusts the example file.

--- example.js
+++ example.js
@@ -18,9 +18,9 @@ puppeteer.use(require('puppeteer-extra-plugin-angular')());
   // Launch the browser and proceed with pool creation.
   await pagePool.launch();
   // Acquire and release the page seamlessly.
-  await pagePool.process(async (page) => {
+  await pagePool.process(async (page, data) => {
     // Navigate to given Url and wait until Angular is ready.
-    await page.navigateUntilReady('https://angular.io');
+    await page.navigateUntilReady(data.url);
     // Selector will find a button on the top of the page that say "Get Started".
     await page.clickIfExists('a.button.hero-cta', 'Top Get Started Button');
     // Should navigate to the new Url.
@@ -29,7 +29,7 @@ puppeteer.use(require('puppeteer-extra-plugin-angular')());
     await page.clickIfExists('a.button.hero-cta', 'Top Get Started Button');
     // Url should be the same
     console.log(page.target().url()); // https://angular.io/start
-  });
+  }, { url: 'https://angular.io' });
   // All done.
   await pagePool.destroy();
 })();
