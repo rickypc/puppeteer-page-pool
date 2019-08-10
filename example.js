c89fea1c6b0cadc4d31eecf7befa2d8f0c5e1b47
Richard Huang <rickypc@users.noreply.github.com>
2019-08-09 22:25:13 -0700

    Adds the RunKit example file.

---
+++ example.js
@@ -0,0 +1,35 @@
+const PagePool = require('puppeteer-page-pool');
+const puppeteer = require('puppeteer-extra');
+puppeteer.use(require('puppeteer-extra-plugin-angular')());
+
+(async () => {
+  const pagePool = new PagePool({
+    // See opts section of https://bit.ly/2GXZbUR
+    poolOptions: {
+      log: true,
+    },
+    puppeteer,
+    // See https://bit.ly/2M6kVCd
+    puppeteerOptions: {
+      // I want to see all the actions :)
+      headless: true,
+    },
+  });
+  // Launch the browser and proceed with pool creation.
+  await pagePool.launch();
+  // Acquire and release the page seamlessly.
+  await pagePool.process(async (page) => {
+    // Navigate to given Url and wait until Angular is ready.
+    await page.navigateUntilReady('https://angular.io');
+    // Selector will find a button on the top of the page that say "Get Started".
+    await page.clickIfExists('a.button.hero-cta', 'Top Get Started Button');
+    // Should navigate to the new Url.
+    console.log(page.target().url()); // https://angular.io/start
+    // Selector won't find any element on this page.
+    await page.clickIfExists('a.button.hero-cta', 'Top Get Started Button');
+    // Url should be the same
+    console.log(page.target().url()); // https://angular.io/start
+  });
+  // All done.
+  await pagePool.destroy();
+})();
