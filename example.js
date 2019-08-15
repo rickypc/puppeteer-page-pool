19dcf9e13bb1b655f8aeacee9923f56e5e2cbb42
Richard Huang <rickypc@users.noreply.github.com>
2019-08-15 13:07:20 -0700

    Adjusts the example to be more clear.

--- example.js
+++ example.js
@@ -6,30 +6,36 @@ puppeteer.use(require('puppeteer-extra-plugin-angular')());
   const pagePool = new PagePool({
     // See opts section of https://bit.ly/2GXZbUR
     poolOptions: {
-      log: true,
+      max: 3,
     },
     puppeteer,
     // See https://bit.ly/2M6kVCd
     puppeteerOptions: {
-      // I want to see all the actions :)
-      headless: true,
+      headless: false,
     },
   });
+
   // Launch the browser and proceed with pool creation.
   await pagePool.launch();
-  // Acquire and release the page seamlessly.
-  await pagePool.process(async (page, data) => {
-    // Navigate to given Url and wait until Angular is ready.
+
+  const promises = [
+    'https://angular.io',
+    'https://www.chromium.org',
+    'https://santatracker.google.com',
+  ].map((url) => pagePool.process(async (page, data) => {
+    // PagePool will acquire and release the page seamlessly.
+    // Navigate to given Url and wait until Angular is ready
+    // if it's an angular page.
     await page.navigateUntilReady(data.url);
-    // Selector will find a button on the top of the page that say "Get Started".
-    await page.clickIfExists('a.button.hero-cta', 'Top Get Started Button');
-    // Should navigate to the new Url.
-    console.log(page.target().url()); // https://angular.io/start
-    // Selector won't find any element on this page.
-    await page.clickIfExists('a.button.hero-cta', 'Top Get Started Button');
-    // Url should be the same
-    console.log(page.target().url()); // https://angular.io/start
-  }, { url: 'https://angular.io' });
+    await page.screenshot({
+      fullPage: true,
+      path: `${data.url.replace(/https?:|\//g, '')}-screenshot.png`,
+    });
+  }, { url }));
+
+  // Wait until it's all done.
+  await Promise.all(promises);
+
   // All done.
   await pagePool.destroy();
 })();
